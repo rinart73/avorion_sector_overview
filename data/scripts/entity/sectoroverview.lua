bf924cb1596b12fac4e0e6b48d2713ef92157d2e
shulrak <shulrak@gmail.com>
2017-02-11 21:32:31 +0000

    Add player list

--- data/scripts/entity/sectoroverview.lua
+++ data/scripts/entity/sectoroverview.lua
@@ -4,10 +4,12 @@ require("utility")
 require("stringutility")
 
 
-local entityList = nil
+local stationList = nil
 local gateList = nil
+local playerList = nil
 local tabMap = {}
 local entities = {}
+local playerAddedList = {}
 local isWindowShowing = false
 local window = nil
 local tabbedWindow = nil
@@ -45,8 +47,8 @@ function initUI()
     local buildTab = tabbedWindow:createTab("Build"%_t, "data/textures/icons/rss.png", "Station List"%_t)
     local hsplit = UIHorizontalSplitter(Rect(vec2(0, 0), tabbedWindow.size ), 10, 0, 0.5)
     hsplit.bottomSize = 40
-    entityList = buildTab:createListBox(hsplit.top)
-    tabMap[buildTab.index] = entityList
+    stationList = buildTab:createListBox(hsplit.top)
+    tabMap[buildTab.index] = stationList
 
     -- ship
     local buildTab = tabbedWindow:createTab("Build"%_t, "data/textures/icons/vortex.png", "Gate List"%_t)
@@ -54,28 +56,109 @@ function initUI()
     hsplit.bottomSize = 40
     gateList = buildTab:createListBox(hsplit.top)
     tabMap[buildTab.index] = gateList
+
+    -- Players
+    local buildTab = tabbedWindow:createTab("Build"%_t, "data/textures/icons/backup.png", "Player List"%_t)
+    local hsplit = UIHorizontalSplitter(Rect(vec2(0, 0), tabbedWindow.size - vec2(0, 55) ), 10, 0, 0.5)
+    hsplit.bottomSize = 70 
+    playerList = buildTab:createListBox(hsplit.top)
+    tabMap[buildTab.index] = playerList 
+
+    local hsplit = UIHorizontalSplitter(hsplit.bottom, 10, 0, 0.5)
+    hsplit.bottomSize = 35
+
+    playerCombo = buildTab:createComboBox(hsplit.top, "")
+
+    local vsplit = UIVerticalSplitter(hsplit.bottom, 10, 0, 0.5)
+
+    addScriptButton = buildTab:createButton(vsplit.left, "Add"%_t, "onAddPlayerToGroupPressed")
+    addScriptButton.tooltip = 
+        [[ Add the selected player from the combo box to the list of players"]]
+    removeScriptButton = buildTab:createButton(vsplit.right, "Remove"%_t, "onRemovePlayerFromGroupPressed")
+    removeScriptButton.tooltip =
+        [[ "Remove the selected player from the list of players."]]
+
+    local player = Player()
+    print ("INIT PLAYER INFO craft : ", player.craftIndex,
+           ", numShips :", player.numShips,
+           "shipname :", player:getShipNames(),
+           "ship pos2 : ",  "END")
+    local x, y = player:getShipPosition(player:getShipNames())
+end
+
+function refreshPlayerList()
+    -- small hack to keep the order of the players consistent on the screen
+    playerList:clear()
+    local playerSortedList = {}
+    for name, isInList in pairs(playerAddedList) do
+        if (isInList) then
+            table.insert(playerSortedList, name)
+        end
+    end
+
+    table.sort(playerSortedList)
+    for _, name in ipairs(playerSortedList) do
+        playerList:addEntry(name)
+    end
+end
+
+function onAddPlayerToGroupPressed()
+    local name = playerCombo.selectedEntry
+    if (not playerAddedList[name]) then
+        playerList:addEntry(name)
+        playerAddedList[name] = true
+        refreshPlayerList()
+    end
+end
+
+function onRemovePlayerFromGroupPressed()
+    local name = playerCombo.selectedEntry
+    if (playerAddedList[name]) then
+        playerAddedList[name] = nil 
+        refreshPlayerList()
+    end
 end
 
 function onShowWindow()
     entities = {}
-    entityList:clear()
+    stationList:clear()
     gateList:clear()
+    playerList:clear()
+    playerCombo:clear()
 
+    -- fill list for station and gates
     local player = Player()
     for index, entity in pairs({Sector():getEntities()}) do
-      if (entity.type == EntityType.Station) then
-        local titleArgs = entity:getTitleArguments()
-        local title =  entity.title % titleArgs
-        local entryName = title .. "    " .. entity.name 
-        entityList:addEntry(entryName)
-        entities[entryName] = entity
-      end
-      if (entity.title and string.match(entity.title, "Gate"))then
-        local entryName = entity.title 
-        gateList:addEntry(entryName)
-        entities[entryName] = entity
-      end
+        if (entity.type == EntityType.Station) then
+            local titleArgs = entity:getTitleArguments()
+            local title =  entity.title % titleArgs
+            local entryName = title .. "    " .. entity.name 
+            stationList:addEntry(entryName)
+            entities[entryName] = entity
+        end
+        if (entity.title and string.match(entity.title, "Gate"))then
+            local entryName = entity.title 
+            gateList:addEntry(entryName)
+            entities[entryName] = entity
+        end
     end
+
+    -- fill player combo box
+    for index, name in pairs(Galaxy():getPlayerNames()) do
+        if player.name:lower() ~= name:lower() then
+            playerCombo:addEntry(name);
+            tabMap[name] = index
+        end
+    end
+
+    refreshPlayerList()
+
+    -- local name = "test"
+    -- for i=1,10  do
+    --     print (name .. i)
+    --     playerCombo:addEntry(name .. i);
+    --     tabMap[name] = i + 10
+    -- end
     isWindowShowing = true
 end
 
@@ -84,19 +167,19 @@ function onCloseWindow()
 end
 
 function updateUI()
-  if not isWindowShowing then
-    return end
+    if not isWindowShowing then
+        return end
 
-  if Mouse():mouseDown(1) then
-    local tabIndex = tabbedWindow:getActiveTab().index
-    local selectedEntry = tabMap[tabIndex]:getSelectedEntry()
-    if (selectedEntry) then
-      local entityToTarget = entities[selectedEntry];
-      if (entityToTarget) then
-        Player().selectedObject = entityToTarget
-      end
+    if Mouse():mouseDown(1) then
+        local tabIndex = tabbedWindow:getActiveTab().index
+        local selectedEntry = tabMap[tabIndex]:getSelectedEntry()
+        if (selectedEntry) then
+            local entityToTarget = entities[selectedEntry];
+            if (entityToTarget) then
+                Player().selectedObject = entityToTarget
+            end
+        end
     end
-  end
 end
 
 
